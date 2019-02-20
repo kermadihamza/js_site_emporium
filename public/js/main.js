d85f4c0b71a9df1cd64a0bf762e7d911cc1ba12a
Hamza <kermadih@gmail.com>
2019-02-20 12:56:32 +0100

    Site presque finit sauf iphone4

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,7 @@ let createAccount = document.getElementsByClassName('createAccount')[0];
 let repUsers = document.getElementById('first_name');
 let btnConnect = document.getElementById('myBtnConnect');
 let off = document.getElementsByClassName('fa-power-off')[0];
+let changeI = document.getElementsByClassName('buttonSlide')[0];
 off.style.display = 'none';
 btnConnect.style.display = 'none'
 register.style.display = 'none';
@@ -25,22 +26,22 @@ console.log(nav);
 
 // Changer la couleur du background en noir
 function changeColor() {
-    if (btnBlack) {
-        body.style.backgroundColor = 'black';
-        nav.classList.remove('bg-light');
-        nav.classList.add('bg-fulldark');
-    }
+  if (btnBlack) {
+    body.style.backgroundColor = 'black';
+    nav.classList.remove('bg-light');
+    nav.classList.add('bg-fulldark');
+  }
 }
 btnBlack.addEventListener('click', changeColor);
 
 
 // Changer la couleur du background en blanc
 function changeColorWhite() {
-    if (btnWhite) {
-        body.style.backgroundColor = 'white';
-        nav.classList.remove('bg-fulldark');
-        nav.classList.add('bg-light');
-    }
+  if (btnWhite) {
+    body.style.backgroundColor = 'white';
+    nav.classList.remove('bg-fulldark');
+    nav.classList.add('bg-light');
+  }
 }
 
 btnWhite.addEventListener('click', changeColorWhite);
@@ -48,92 +49,95 @@ btnWhite.addEventListener('click', changeColorWhite);
 
 // Changer la couleur du texte .change en blanc
 function changeColorW() {
-let colorChange = document.querySelectorAll('.change');
-    for (i = 0; i < colorChange.length; i++) {
-        colorChange[i].classList.remove('text-dark');
-        colorChange[i].classList.remove('text-secondary');
-        colorChange[i].classList.add('text-white');
-        emporium.style.color = 'white';
-        emporiumScroll.style.color = "white";
-    }
+  let colorChange = document.querySelectorAll('.change');
+  for (i = 0; i < colorChange.length; i++) {
+    colorChange[i].classList.remove('text-dark');
+    colorChange[i].classList.remove('text-secondary');
+    colorChange[i].classList.add('text-white');
+    emporium.style.color = 'white';
+    emporiumScroll.style.color = "white";
+    changeI.style.color = 'white';
+  }
 }
 btnBlack.addEventListener('click', changeColorW);
 
 
 // Changer la couleur du texte .change en couleur initial
 function changeColorD() {
-    let colorChange = document.querySelectorAll('.change');
-        for (i = 0; i < colorChange.length; i++) {
-            colorChange[i].classList.remove('text-white');
-            emporium.style.color = 'black';
-            emporiumScroll.style.color = 'black';
+  let colorChange = document.querySelectorAll('.change');
+  for (i = 0; i < colorChange.length; i++) {
+    colorChange[i].classList.remove('text-white');
+    colorChange[i].classList.add('text-dark');
+    emporium.style.color = 'black';
+    emporiumScroll.style.color = 'black';
+    changeI.style.color = 'black';
 
-        }
-    }
-    btnWhite.addEventListener('click', changeColorD);
-
-
-
-    let navB = document.querySelector('#navigation');
-    let navTop = nav.offsetTop;
-    let square1 = document.getElementsByClassName('square1')[0];   
- let square2 = document.getElementsByClassName('square2')[0];
-    
-    function stickyNavigation() {
-      console.log('navTop = ' + navTop);
-      console.log('scrollY = ' + window.scrollY);
-      
-      if (window.scrollY >= navTop) {
-          // nav offsetHeight = height of nav
-          document.body.style.paddingTop = navB.offsetHeight + 'px';
-          square1.style.display = 'none';
-          square2.style.display = 'none';
-          btn.style.display = 'none';
-          document.body.classList.add('fixed-nav');
-          emporiumScroll.style.display = '';
-      } else {
-        document.body.classList.remove('fixed-nav');
-        emporiumScroll.style.display = 'none';
-        square1.style.display = '';
-        square2.style.display = '';
-        btn.style.display = '';
-        // emporium.classList.remove('pl-3');
-        // emporium.classList.add('text-center');
-      }
-    }
-    
-    window.addEventListener('scroll', stickyNavigation);
+  }
+}
+btnWhite.addEventListener('click', changeColorD);
 
 
-    var modal = document.getElementById('myModal');
-    var btn = document.getElementById("myBtn");
-    var span = document.getElementsByClassName("close")[0];
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+
+let navB = document.querySelector('#navigation');
+let navTop = nav.offsetTop;
+let square1 = document.getElementsByClassName('square1')[0];
+let square2 = document.getElementsByClassName('square2')[0];
+
+function stickyNavigation() {
+  console.log('navTop = ' + navTop);
+  console.log('scrollY = ' + window.scrollY);
+
+  if (window.scrollY >= navTop) {
+    // nav offsetHeight = height of nav
+    document.body.style.paddingTop = navB.offsetHeight + 'px';
+    square1.style.display = 'none';
+    square2.style.display = 'none';
+    btn.style.display = 'none';
+    document.body.classList.add('fixed-nav');
+    emporiumScroll.style.display = '';
+  } else {
+    document.body.classList.remove('fixed-nav');
+    emporiumScroll.style.display = 'none';
+    square1.style.display = '';
+    square2.style.display = '';
+    btn.style.display = '';
+    // emporium.classList.remove('pl-3');
+    // emporium.classList.add('text-center');
+  }
+}
+
+window.addEventListener('scroll', stickyNavigation);
+
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+span.onclick = function () {
+  modal.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 function create() {
   if (createAccount) {
     register.style.display = '';
-  signIn.style.display = 'none';
+    signIn.style.display = 'none';
   }
 }
 createAccount.addEventListener('click', create);
 
 function registerOk() {
-    if (btnRegister) {
-      signIn.style.display = '';
-      register.style.display = 'none';
-    }
+  if (btnRegister) {
+    signIn.style.display = '';
+    register.style.display = 'none';
+  }
 }
 btnRegister.addEventListener('click', registerOk);
 
@@ -142,12 +146,12 @@ function connexion() {
   if (btnConnexion) {
     btn.style.display = 'none'
     event.preventDefault();
-    modal.style.display = 'none'    
+    modal.style.display = 'none'
     btnConnect.style.display = '';
     btnConnect.innerHTML = repUsers.value + ` <i class="fas fa-user"></i>`
     off.style.display = '';
-    
-}
+
+  }
 }
 btnConnexion.addEventListener('click', connexion);
 
@@ -170,7 +174,7 @@ off.addEventListener('click', deco);
 
 // function oneOne() {
 //   if (next) {
-    
+
 //   } 
 // }
 // next.addEventListener('click', oneOne)
@@ -208,7 +212,7 @@ function slide(event) {
   slideDiv.style.transform = 'translateX(' + -14.28 * pos + '%)'
   // slideDiv.style.transform = `translateX( ${-14.28 * pos}%)`
   slideDiv.style.transition = 'all 1s ease'
-    event.preventDefault()
+  event.preventDefault()
 }
 
 buttons0.addEventListener('click', slide);
